e6a899fc4b003a86087adaef4573a60cd861a30b
Seikened <fernanlee2131@gmail.com>
2024-02-02 16:48:05 -0600

    Delete unnecessary files and remove unused code

--- Progra/tareas/02A_pulgadasCentimetros.py
+++
@@ -1,5 +0,0 @@
-def PulgadasCentimetros(pulgadas):
-    return 2.54 * pulgadas
-
-pulgadasUser = PulgadasCentimetros(float(input("Introduce tus pulgadas a converir: ")))
-print(f"Estas son las pulgadas: {pulgadasUser}")
