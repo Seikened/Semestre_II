0eb0d9638c8506aa48851e08649a0756950cf72d
Seikened <fernanlee2131@gmail.com>
2024-02-10 14:56:03 -0600

    Add createTable function to create a new table in the database

--- 01_MIS_RETOS/SQLite/proyectoBD.py
+++ 01_MIS_RETOS/SQLite/proyectoBD.py
@@ -6,4 +6,10 @@ def createDB():
     conn.close()
 
 
+def createTable():
+    conn = sql.connect("casas.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        """CREATE TABLE"""
+    )
 createDB()
