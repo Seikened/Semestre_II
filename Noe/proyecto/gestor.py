7d3b272905df5c6d9bee6bd954df25ef036b23c0
Seikened <fernanlee2131@gmail.com>
2024-02-21 14:50:24 -0600

    Refactored code to import functions locally and added a Solve function for solving linear equations using LU, Jacobi, or Gauss-Seidel methods

--- Noe/proyecto/gestor.py
+++ Noe/proyecto/gestor.py
@@ -1,41 +1,60 @@
-from Noe.proyecto.funciones import *
+from funciones import *
 
 
-#=========================================== test =========================================
-
-
-
-
-A = np.array([[2, -1, 0],
-              [-1, 3, -1],
-              [0, -1, 2]])
-
-b = np.array([[1],
-              [8],
-              [-5]])
-
-# # Utilizamos descomposición LU para resolver Ax = b
-# L, U = descomposicion_lu(A)
-# # print("L:", L)
-# # print("U:", U)
-
-# # # Resolver Lz = b para obtener z
-# z = solve_Lz(L, b)
-# #print("z:", z)
-
-# # # Resolver Ux = z para obtener x
-# x = solve_Ux(z, U)
-# print("Solución x usando LU:", x)
-
-# # # Probamos el método de Jacobi
-# x_inicial = np.zeros((A.shape[0], 1))
-# x_jacobi, iter_jacobi = jacobi_m(A, b[:,0], x_inicial)
-# print("Solución x usando Jacobi:", x_jacobi, "en", iter_jacobi, "iteraciones")
+#=========================================== MENU =========================================
 
 
 
-# Probamos el método de Gauss-Seidel
 
-x_inicial = np.zeros((A.shape[0], 1))
-x_gauss_seidel, iter_gauss_seidel = GaussSeidel_m(A, b, x_inicial)
-print("Solución x usando Gauss-Seidel:", x_gauss_seidel, "en", iter_gauss_seidel, "iteraciones")
+def Solve(tipoMetodo,A,b):
+    tipoMetodo = tipoMetodo.lower()
+    
+    A = np.array(A)
+
+    b = np.array(b)
+    match tipoMetodo:
+        
+        # CASO LU
+        case "lu":
+            # Utilizamos descomposición LU para resolver Ax = b
+            L, U = descomposicion_lu(A)
+            # Resolver Lz = b para obtener z
+            z = solve_Lz(L, b)
+            # Resolver Ux = z para obtener x
+            x = solve_Ux(z, U)
+            print("Solución x usando LU:", x)
+
+        # CASO JACOBI
+        case "jacobi":
+            # Probamos el método de Jacobi
+            x_inicial = np.zeros((A.shape[0], 1))
+            x_jacobi, iter_jacobi = jacobi_m(A, b[:,0], x_inicial)
+            print("Solución x usando Jacobi:", x_jacobi, "en", iter_jacobi, "iteraciones")
+
+        # CASO SEIDEL
+        case "seidel":
+            # Probamos el método de Gauss-Seidel
+            x_inicial = np.zeros((A.shape[0], 1))
+            x_gauss_seidel, iter_gauss_seidel = GaussSeidel_m(A, b, x_inicial)
+            print("Solución x usando Gauss-Seidel:", x_gauss_seidel, "en", iter_gauss_seidel, "iteraciones")
+
+
+
+
+
+#================================== TEST ==================================
+
+
+
+
+a = [[2, -1, 0],
+    [-1, 3, -1],
+    [0, -1, 2]]
+
+b = [[1],
+    [8],
+    [-5]]
+
+tipo = "seidel"
+
+Solve(tipo,a,b)
